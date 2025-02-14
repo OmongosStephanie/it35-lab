3d012f43ec22133be78c369279fc9168bc1c208e
stephanieomongos05@gmail.com <stephanieomongos05@gmail.com>
2025-02-14 12:13:05 +0800

    Added Details Page/Updated

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -18,13 +18,14 @@ import {
   import { Redirect, Route } from 'react-router';
   import Home from './Home';
   import About from './About';
+  import Details from './Details';
 
   const Menu: React.FC = () => {
       const path = [
           {name:'Home', url: '/it35-lab/app/home', icon: homeOutline},
           {name:'About', url: '/it35-lab/app/about', icon: rocketOutline},
       ]
-      
+
       return (
           <IonPage>
           <IonSplitPane contentId="main">
@@ -45,23 +46,26 @@ import {
                               </IonItem>
                           </IonMenuToggle>
                       ))}
+
                       {/*Logout Button*/}
                       <IonButton routerLink="/it35-lab" routerDirection="back" expand="full">
                           <IonIcon icon={logOutOutline} slot="start"> </IonIcon>
                       Logout
                       </IonButton>
+
                   </IonContent>
               </IonMenu>
 
               <IonRouterOutlet id="main">
                   <Route exact path="/it35-lab/app/home" component={Home} />
+                  <Route exact path="/it35-lab/app/home/details" component={Details} />
                   <Route exact path="/it35-lab/app/about" component={About} />
 
                   <Route exact path="/it35-lab/app">
                       <Redirect to="/it35-lab/app/home"/>
                   </Route>
               </IonRouterOutlet>
-              
+
           </IonSplitPane>
       </IonPage>
   );
