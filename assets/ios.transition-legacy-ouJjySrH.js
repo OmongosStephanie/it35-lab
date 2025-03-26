f70d3d12fcffdf98320e5b5f553fd05293e9ed8c
steph <stephanieomongos05@gmail.com>
2025-03-27 00:38:11 +0800

    Updates

--- assets/ios.transition-legacy-ouJjySrH.js
+++ assets/ios.transition-legacy-ouJjySrH.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-QVNTdkXd.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-DdOMGL59.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
