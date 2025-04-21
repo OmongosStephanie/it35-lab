73c304a5a3313a57581a42b20bd87d27dd4ec951
tepani <stephanieomongos05@gmail.com>
2025-04-21 13:40:26 +0800

    Updates

--- assets/ios.transition-legacy-B1dJT0lL.js
+++ assets/ios.transition-legacy-B1dJT0lL.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-S8wm8hP1.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-Bjw_xI2K.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/md.transition-legacy-B-a71Np9.js
+++ assets/md.transition-legacy-B-a71Np9.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-S8wm8hP1.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-Bjw_xI2K.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
