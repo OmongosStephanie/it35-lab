3fed446e2b0aef13f73aa27ad812694fa3f96d80
tepani <stephanieomongos05@gmail.com>
2025-04-21 13:37:09 +0800

    updated feed

--- src/components/FeedContainer.tsx
+++ src/components/FeedContainer.tsx
@@ -160,7 +160,7 @@ const FeedContainer = () => {
                 </IonCardHeader>
   
                 <IonCardContent>
-                  <IonText style={{ color: 'black' }}>
+                  <IonText style={{ color: 'White' }}>
                     <h1>{post.post_content}</h1>
                   </IonText>
                 </IonCardContent>
