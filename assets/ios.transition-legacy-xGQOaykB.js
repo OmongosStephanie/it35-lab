ee0664313d39281249b79450abb47b931ca11236
steph <stephanieomongos05@gmail.com>
2025-04-27 13:41:06 +0800

    Updates

--- assets/ios.transition-legacy-xGQOaykB.js
+++ assets/ios.transition-legacy-xGQOaykB.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Bjw_xI2K.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-cH_OsxLv.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-gzb5zye6.js
+++ assets/swipe-back-legacy-gzb5zye6.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Bjw_xI2K.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-cH_OsxLv.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
